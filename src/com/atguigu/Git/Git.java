542adbf53dd6caed0cc22cc033562ea5b67ba114
xiaokang502899 <xiaokang502899@163.com>
2019-12-14 19:00:28 +0800

    中国牛逼！！！

--- src/com/atguigu/Git/Git.java
+++ src/com/atguigu/Git/Git.java
@@ -3,9 +3,9 @@ package com.atguigu.Git;
 public class Git {
 public static void main(String[] args) {
 	System.out.println("主席好！！");
-	System.out.println("dsffds");
-	System.out.println("222222");
-	System.out.println("11111");
-	System.err.println("bbbbbb");
+	System.out.println("同志们好！！");
+	System.out.println("同志们辛苦了！");
+	System.out.println("为人民服务");
+	System.err.println("时刻准备着");
 }
 }
